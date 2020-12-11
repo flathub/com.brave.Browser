f9c54dbbd1ecb4878e61d97ac880c573665268ed
rany2 <rany2@riseup.net>
2020-12-11 20:58:55 +0200

    Rebase on Chrome Flatpak to resolve issue with Zypak and Flextop

--- brave.sh
+++ brave.sh
@@ -1,9 +1,45 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 
-# Show warning for people that don't have expose-pids support every time
-if ! zypak-helper spawn-strategy-test; then
-	set /app/share/flatpak-brave/mimic_warning.html chrome://welcome "$@"
+# Merge the policies with the host ones.
+policy_root=/etc/chromium/policies
+
+for policy_type in managed recommended; do
+  policy_dir="$policy_root/$policy_type"
+  mkdir -p "$policy_dir"
+
+  if [[ "$policy_type" == 'managed' ]]; then
+    ln -sf /app/share/flatpak-brave/flatpak_policy.json "$policy_dir"
+  fi
+
+  if [[ -d "/run/host/$policy_root/$policy_type" ]]; then
+    find "/run/host/$policy_root/$policy_type" -name '*.json' \
+      -exec ln -sf '{}' "$policy_root/$policy_type" \;
+  fi
+done
+
+# Determine if to show the first run page & the mimic strategy warning.
+mimic_stamp="$XDG_DATA_HOME/flatpak-brave-mimic-stamp"
+
+if [[ ! -f "$mimic_stamp" ]] && ! zypak-helper spawn-strategy-test; then
+  zenity --info --title='Brave Flatpak' --no-wrap \
+    --text="$(< /app/share/flatpak-brave/mimic_warning.txt)"
+  touch "$mimic_stamp"
 fi
 
+if [[ -f "$XDG_CONFIG_HOME/chrome-flags.conf" ]]; then
+  IFS=$'\n'
+  flags=($(grep -v '^#' "$XDG_CONFIG_HOME/chrome-flags.conf"))
+  unset IFS
+
+  set -- "${flags[@]}" "$@"
+fi
+
+# Chrome loads cursors by itself, following the standard XCursor search
+# directories. However, the fd.o runtime patches XCursor to look in
+# $XDG_DATA_DIRS, but Chrome's own loading of course does not follow that.
+# Therefore, we manually set the XCursor path to follow $XDG_DATA_DIRS here.
+export XCURSOR_PATH=$(echo "$XDG_DATA_DIRS" | sed 's,\(:\|$\),/icons\1,g')
+export CHROME_WRAPPER=$(readlink -f "$0")
 export TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID"
+
 exec zypak-wrapper.sh /app/brave/brave-browser "$@"
