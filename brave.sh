eec7989ab4ea8e9a220fc3ccb42c5c2ae517a871
cthu1hoo <47687909+cthu1hoo@users.noreply.github.com>
2022-01-29 11:41:21 +0300

    Support sourcing host policy files from /etc/brave into sandbox. (#105)
    Support sourcing host policy files from /etc/brave into sandbox.
    
    Co-authored-by: Andrew Dolgov <noreply@fakecake.org>
    Co-authored-by: rany <ranygh@riseup.net>

---
+++ brave.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+# Merge the policies with the host ones.
+policy_root=/etc/brave/policies
+
+for policy_type in managed recommended; do
+  policy_dir="$policy_root/$policy_type"
+  mkdir -p "$policy_dir"
+
+  if [[ "$policy_type" == 'managed' ]]; then
+    ln -sf /app/share/flatpak-chrome/flatpak_policy.json "$policy_dir"
+  fi
+
+  if [[ -d "/run/host/$policy_root/$policy_type" ]]; then
+    find "/run/host/$policy_root/$policy_type" \
+      -maxdepth 1 -name '*.json' -type f \
+      -exec ln -sf '{}' "$policy_root/$policy_type" \;
+  fi
+done
+
+exec cobalt "$@"
