1a1e4088083c75bb21210be24359c5d331500f82
Evan Anderson <evan@eaanderson.com>
2022-05-30 21:03:01 -0500

    Inherit enrollment configuration from host etc
    Chrome cloud management uses files in /etc/opt/chrome/policies/enrollment to
    enroll endpoint browsers in management. Link these in addition to any policy files.
    
    Ref: https://support.google.com/chrome/a/answer/9301891?hl=en&ref_topic=9301744#zippy=%2Cenroll-browsers-on-linux
    Signed-off-by: rany <ranygh@riseup.net>

--- brave.sh
+++ brave.sh
@@ -3,7 +3,7 @@
 # Merge the policies with the host ones.
 policy_root=/etc/brave/policies
 
-for policy_type in managed recommended; do
+for policy_type in managed recommended enrollment; do
   policy_dir="$policy_root/$policy_type"
   mkdir -p "$policy_dir"
 
@@ -12,7 +12,7 @@ for policy_type in managed recommended; do
   fi
 
   if [[ -d "/run/host/$policy_root/$policy_type" ]]; then
-    find "/run/host/$policy_root/$policy_type" \
+    find "/run/host/$policy_root/$policy_type" -type f -name '*' \
       -maxdepth 1 -name '*.json' -type f \
       -exec ln -sf '{}' "$policy_root/$policy_type" \;
   fi
